fde6d7898aafef26e0788a09973b8c7a117432ae
Benjamin Hanzelmann <benjamin@hanzelmann.de>
2024-11-19 23:44:22 +0100

    Enable conjure to execute from comment blocks in clojure

--- config/lua/plugins/conjure.lua
+++ config/lua/plugins/conjure.lua
@@ -1,8 +1,9 @@
 -- [nfnl] Compiled from fnl/plugins/conjure.fnl by https://github.com/Olical/nfnl, do not edit.
 local function _1_()
   vim.g["conjure#mapping#doc_word"] = "K"
-  vim.g["conjure#client#clojure#nrepl#eval#auto_require"] = false
-  vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = false
+  vim.g["conjure#client#clojure#nrepl#eval#auto_require"] = true
+  vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = true
+  vim.g["conjure#eval#gsubs"] = {["do-comment"] = {"^%(comment[%s%c]", "(do "}}
   vim.g["conjure#client#sql#stdio#command"] = "psql -U nabcos postgres"
   return nil
 end
